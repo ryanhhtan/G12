636018957e4b66ccd58c4e99ff6db367524b0398
Ryan <tanhaihuacn@hotmail.com>
2016-02-15 01:05:46 -0800

    fix footer link in script

--- scripts/addheaderfooter_jquery.js
+++ scripts/addheaderfooter_jquery.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
 
    // jQuery methods go here...
    $("#header").load("http://www.1536g12.byethost9.com/html/header.html");
-   $("#footer").load("http://www.1536g12.byethost9.com/footer.html");
+   $("#footer").load("http://www.1536g12.byethost9.com/html/footer.html");
 });

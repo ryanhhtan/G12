b6750f143380abd365fb222f1d4d17da1feddac1
Ryan <tanhaihuacn@hotmail.com>
2016-02-14 19:41:21 -0800

    add footer

--- scripts/addheaderfooter_jquery.js
+++ scripts/addheaderfooter_jquery.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
 
    // jQuery methods go here...
    $("#header").load("http://localhost:11111/html/header.html");
-
+   $("#footer").load("http://localhost:11111/html/footer.html");
 });

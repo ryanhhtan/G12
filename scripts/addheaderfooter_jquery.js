fbccdf5927a6c5950e16c337b3c10f3dd507ed15
ryantanhh <tanhaihuacn@hotmail.com>
2016-02-22 08:27:32 -0800

    fixed path

--- scripts/addheaderfooter_jquery.js
+++ scripts/addheaderfooter_jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
    // jQuery methods go here...
-   $("#header").load("/A00950721/G12/html/header.html");
-   $("#footer").load("/A00950721/G12/html/footer.html");
+   $("#header").load("http://www.1536g12.byethost9.com/A00950721/G12/html/header.html");
+   $("#footer").load("http://www.1536g12.byethost9.com/A00950721/G12/html/footer.html");
 });

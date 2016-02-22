3390febf63ec82b5bd735d03a2606aff25a1e2d1
ryantanhh <tanhaihuacn@hotmail.com>
2016-02-21 23:03:50 -0800

    change path

--- scripts/addheaderfooter_jquery.js
+++ scripts/addheaderfooter_jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
    // jQuery methods go here...
-   $("#header").load("http://www.1536g12.byethost9.com/html/header.html");
-   $("#footer").load("http://www.1536g12.byethost9.com/html/footer.html");
+   $("#header").load("http://www.1536g12.byethost9.com/G12/html/header.html");
+   $("#footer").load("http://www.1536g12.byethost9.com/G12/html/footer.html");
 });

f6c9bff87982c1e7d73296bc6a8f30c5415a5ad0
ryantanhh <tanhaihuacn@hotmail.com>
2016-02-22 01:11:06 -0800

    modify the path as in BCITDEV

--- scripts/addheaderfooter_jquery.js
+++ scripts/addheaderfooter_jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
    // jQuery methods go here...
-   $("#header").load("/G12/html/header.html");
-   $("#footer").load("/G12/html/footer.html");
+   $("#header").load("/A00950721/G12/html/header.html");
+   $("#footer").load("/A00950721/G12/html/footer.html");
 });

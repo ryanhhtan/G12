7c700d27f96956ba86f46aae6b7cd39b2f2a7d20
ryantanhh <tanhaihuacn@hotmail.com>
2016-03-27 18:07:37 -0700

    fix the footer student name.

--- php/footer.php
+++ php/footer.php
@@ -5,7 +5,7 @@
 				<div id="footertext">
 				
 					<!--declaimer-->
-					<p> Copyright &copy; 2016 | Designed by Victor, Ebon Hugh, Minkai and Ryan</p>
+					<p> Copyright &copy; 2016 | Designed by Victor, Ebon, Hugh, Minkai and Ryan</p>
 					<p> This websit is for learning web design, not for any commercial purpose. Some materials are from internet. If there is any copyright issue, please <a href="../html/contact.html">contact</a> us</p>
 					<!--webpage validation message-->
 					<p>

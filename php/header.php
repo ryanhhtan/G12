44235fec733cb2d847b449b89df138c24b426c56
liang611zh <zhaoliangbz@gmail.com>
2016-04-02 12:53:33 -0700

    change the name of  navigation

--- php/header.php
+++ php/header.php
@@ -37,8 +37,8 @@
 				<div id="menubar">
 					<ul id="mainmenu">
 						<li id="homepage"><a href="../index.php" title="homepage">Home</a></li>
-						<li id="studyinfomenu"><a href="../html/schools.html" title="Schools Info">Study Info</a></li>
-						<li id="livinginfomenu"><a href="../html/livingHomepage.html" title="Schools Info">Living Info</a></li>
+						<li id="studyinfomenu"><a href="../html/schools.html" title="Schools Info">Study</a></li>
+						<li id="livinginfomenu"><a href="../html/livingHomepage.html" title="Schools Info">Living</a></li>
 						<li id="interactingmenu">Interacting</li>
                         <li id="aboutmenu"><a href="../html/about.html" title="about">About Us</a></li>
 						<li id="sitemapmenu"><a href="../html/sitemap.html" title="sitemap">Sitemap</a></li>
